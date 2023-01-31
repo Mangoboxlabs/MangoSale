c429944c287f33426ba8e127c6ddded17de1ec70
WHPC <wh@wh.com>
2023-01-31 20:22:06 +0800

    a

--- contract/mango_airdrop/lib.rs
+++ contract/mango_airdrop/lib.rs
@@ -1,49 +1,144 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 extern crate alloc;
 use ink_lang as ink;
+
 #[allow(unused_imports)]
 #[allow(renamed_and_removed_lints)]
 #[ink::contract]
 mod mango_airdrop {
+    use std::convert::{TryFrom,TryInto};
     use erc20::Erc20;
     use alloc::string::String;
     use ink_prelude::vec::Vec;
     use ink_storage::{
         collections::HashMap as StorageHashMap,
+        traits::{
+            PackedLayout,
+            SpreadLayout,
+        },
     };
-    const CONTRACT_INIT_BALANCE: u128 = 1000 * 1_000_000_000_000;
+    #[derive(Debug,scale::Encode, scale::Decode, Clone, SpreadLayout, PackedLayout)]
+    #[cfg_attr(
+    feature = "std",
+    derive(scale_info::TypeInfo, ink_storage::traits::StorageLayout)
+    )]
+    /**
+    @member owner Airdrop owner
+    @member title Airdrop title
+    @member information Airdrop information
+    @member description Airdrop  description
+    @member token Airdrop  token
+     */
+    pub struct AirdropDetail {
+        id:u128,
+        owner:AccountId,
+        title: String,
+        information: String,
+        description: String,
+        token:AccountId,
+        start_time:u64
+    }
+    #[derive(Debug,scale::Encode, scale::Decode, Clone, SpreadLayout, PackedLayout)]
+    #[cfg_attr(
+    feature = "std",
+    derive(scale_info::TypeInfo, ink_storage::traits::StorageLayout)
+    )]
+    pub struct DistributionInformation {
+        account:AccountId,
+        amount:u128,
+        is_extract:bool
+    }
     #[ink(storage)]
     pub struct MangoAirdrop {
-        user_tokens: StorageHashMap<AccountId, Vec<AccountId>>
+        all_airdrops: Vec<AirdropDetail>,
+        user_airdrops: StorageHashMap<AccountId, Vec<AirdropDetail>>,
+        id_airdrops: StorageHashMap<u128, AirdropDetail>,
+        user_distribution:StorageHashMap<(u128,AccountId),DistributionInformation>
     }
 
     impl MangoAirdrop {
         #[ink(constructor)]
         pub fn new() -> Self {
             Self {
-                user_tokens:StorageHashMap::new()
+                all_airdrops:Vec::new(),
+                user_airdrops:StorageHashMap::new(),
+                id_airdrops:StorageHashMap::new(),
+                user_distribution:StorageHashMap::new(),
             }
-        }
-        #[ink(message)]
-        pub fn new_airdrop(
-            &mut self,
-            erc20_address:AccountId,
-            title:String,
-            information:String
-        ) -> AccountId {
-            let mut user_tokens = self.user_tokens.get(&owner).unwrap_or(&Vec::new()).clone();
-            user_tokens.push(contract_addr);
-            self.user_tokens.insert(owner,user_tokens);
-            contract_addr
         }
         /**
         @notice
-        Get user's tokens
-        @param owner The address to set as the owner of the token. The token  will be owned by this address.
+        Get add a airdrops
+        @member title Airdrop title
+        @member information Airdrop information
+        @member description Airdrop  description
+        @member token Airdrop  token
          */
         #[ink(message)]
-        pub fn get_user_tokens(&self, owner: AccountId) -> Vec<AccountId> {
-            self.user_tokens.get(&owner).unwrap_or(&Vec::new()).clone()
+        pub fn new_airdrop(
+            &mut self,
+            token:AccountId,
+            title:String,
+            information:String,
+            description:String,
+            start_time:u64
+        ) -> bool {
+            let new_id = self.all_airdrops.clone().len() + 1;
+            let new_airdrop = AirdropDetail{
+                id:u128::try_from(new_id).unwrap(),
+                owner:self.env().caller(),
+                title,
+                information,
+                description,
+                token,
+                start_time
+            };
+            self.all_airdrops.push(new_airdrop.clone());
+            let mut user_info = self.user_airdrops.get(&self.env().caller()).unwrap_or(&Vec::new()).clone();
+            user_info.push(new_airdrop.clone());
+            self.user_airdrops.insert(self.env().caller(),user_info);
+            self.id_airdrops.insert(u128::try_from(new_id).unwrap(),new_airdrop);
+            true
+        }
+        /**
+        @notice
+        Allocate funds to users
+        @param id The id of airdrops
+        @param information Details of assignment
+         */
+        #[ink(message)]
+        pub fn distribution(&mut self, id:u128,information:Vec<DistributionInformation>) -> bool {
+
+            let airdrop = self.id_airdrops.get(&id).unwrap().clone();
+            let mut erc20: Erc20 = ink_env::call::FromAccountId::from_account_id(airdrop.token);
+            assert!(airdrop.owner == self.env().caller());
+            let mut all_amount = 0;
+            for i in 0..information.len()  {
+                let account = information[i].account.clone();
+                all_amount+=information[i].amount.clone();
+                self.user_distribution.insert((id,account),information[i].clone());
+            }
+            let _ret = erc20.transfer_from(self.env().caller(),self.env().account_id(),all_amount);
+            true
+        }
+
+        /**
+        @notice
+        Get user's airdrops
+        @param owner The address to set as the owner of the airdrops. The airdrops  will be owned by this address.
+         */
+        #[ink(message)]
+        pub fn get_user_airdrops(&self, owner: AccountId) -> Vec<AirdropDetail> {
+            self.user_airdrops.get(&owner).unwrap_or(&Vec::new()).clone()
+        }
+        /**
+        @notice
+        Get user's airdrops
+        @param owner The address to set as the owner of the airdrops. The airdrops  will be owned by this address.
+         */
+        #[ink(message)]
+        pub fn get_all_airdrops(&self) -> Vec<AirdropDetail> {
+            self.all_airdrops.clone()
         }
     }
 }
