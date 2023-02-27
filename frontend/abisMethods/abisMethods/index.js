d7f2abc323381a46119cad5623cf003d0e509489
city <MangoBOXLabs@gmail.com>
2023-02-27 16:30:34 +0800

    del

--- frontend/abisMethods/abisMethods/index.js
+++
@@ -1,5 +0,0 @@
-import tokenFactory from "./token_factory";
-import mangoLock from "./mango_lock";
-import mango_airdrop from "./mango_airdrop";
-
-export default {tokenFactory, mangoLock, mango_airdrop,}
